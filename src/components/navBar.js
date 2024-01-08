2f96ee67bc0d5c7f09770455ff106f411bbf62a8
Shubham Dubey <subham02101997@gmail.com>
2024-01-09 00:22:45 +0530

    Make responsove front page, add navigation for smaller screen

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-const NavBar = () =>{
-    const  navigate = useNavigate();
-
-    return(
+const NavBar = () => {
+    const navigate = useNavigate();
+    const [display, setDisplay] = useState(false)
+    const handleNavigation = () => {
+        setDisplay(!display)
+    }
+    useEffect(() => {
+        setDisplay(false)
+    }, [navigate])
+    return (
         <div className="nav-bar">
             <div className="left-nav">
-                LOGO HERE
+                SHUBHAM
             </div>
             <div className="right-nav">
+                <span onClick={() => navigate("/home")}>Home</span>
+                <span>About</span>
+                <span onClick={() => navigate("/home/experiments")}>Experiments</span>
+                <span>Products</span>
+                <span>Contact</span>
+            </div>
+            {/* Hamburger after */}
+            <div onClick={handleNavigation} className="hamburger-container">
+                <GiHamburgerMenu />
+            </div>
+            <div style={{
+                display: display ? 'flex' : 'none'
+            }} className="navigation-dropdown">
                 <span onClick={() => navigate("/home")}>Home</span>
                 <span>About</span>
                 <span onClick={() => navigate("/home/experiments")}>Experiments</span>
